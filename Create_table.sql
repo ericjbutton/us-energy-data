66a807913ea9a70869577b0c67c2b7aa94281be5
Akhil Bandi <akhil.bandi1314@gmail.com>
2022-10-04 20:43:01 -0500

    Created table and loaded data into DB.

---
+++ Create_table.sql
@@ -0,0 +1,17 @@
+drop table energy_consumption
+
+CREATE TABLE energy_consumption (
+year INT,
+state TEXT,
+Total_energy bigint,
+gdp real,
+biomass_consumption bigint,
+coal_consumption bigint,
+electricity_consumption bigint,
+fossil_fuel_consumption bigint,
+natural_gas_consumption bigint,	
+primary key(year, state)
+);
+
+select * from energy_consumption
+
