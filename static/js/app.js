a6972dcdb6e1cdae1579b16f0a4a0783ea2d60ff
Sung Ahn <ahnxx132@umn.edu>
2022-10-08 06:18:36 -0500

    updated app.js. bar chart working progress. have a lay out shown on the index.html.

--- static/js/app.js
+++ static/js/app.js
@@ -8,7 +8,7 @@ function InitDashboard() {
     let selector = d3.select("#selDataset"); 
     d3.json(mydata).then((data) => {
         console.log("Here's the data:", data); 
-        
+        // unique values
         let uniquesampleYears = [...new Set(data.map(object => object.year))];
         console.log("Here are the sample years:", uniquesampleYears); // 2010, 2011, 2012, 2013, 2014
         // Populate the dropdown box
@@ -24,19 +24,108 @@ function InitDashboard() {
 
         // Draw the bargraph for the selected sample id. pass in my initialId.
         // calling a function.
-        DrawBargraph(initialId); 
+        Bargraph(initialId); 
 
         // Draw the bubblechart for the selected sample id
-        Drawpiechart(initialId); 
+        piechart(initialId); 
 
         // Show the metadata for the selected sample id 
-        ShowMetadata(initialId); 
+        Metadata(initialId); 
 
         // Show the gauge
-        DrawGauge(initialId); 
+        // Gauge(initialId); 
 
     }); 
 }
+
+
+// ----filter----
+// function called Samples that does: each time I land on an element, calling that object, and each time it does i want to return the id of that sample equals my sampleId.
+// samples.filter looks at element by element by element down the array called samples. value gets appended into resultArray.
+
+function Bargraph(years)
+{
+    console.log(`Bargraph(${years})`); 
+
+    d3.json(mydata).then((data) => {
+        // think about using zip to have an array for GDP vs state plot.
+        // let gdp = data.map(object => object.gdp );
+        // let years = data.map(object => object.year);
+        // let zip = gdp.map(function(e, i) { return [e,  years[i]]
+        // });
+        let gdpdata = data.map(object => object);
+        console.log("Here's the GDP data=", gdpdata);
+        // filter
+        function yearfilter(object) {return object.year == years}; //pulls out the id of 940
+        let yearArray = gdpdata.filter(yearfilter); //and puts it into the array
+        console.log("Here is my yearArray:", yearArray);
+        let gdpArray = yearArray.map(object => object.gdp);
+        console.log("Here is my GDPresultArray:", gdpArray);
+        let twentytenresult = gdpArray[0]; //gives me the first value in this array. 
+        console.log("Alabama GDP value:", twentytenresult);
+        
+        //states
+        let StateArray = yearArray.map(object => object.state);
+        console.log("Here is my 2010 States Array:", StateArray);
+
+
+
+let title = "United States GDP per capita"
+
+let xstates = StateArray
+
+let yGDP = gdpArray
+
+let trace1 = {
+  x: xstates,
+  y: yGDP,
+  type: 'bar'
+};
+
+let data1 = [trace1];
+
+let layout = {
+  title: title
+};
+
+Plotly.newPlot("bar", data1, layout);
+
+    }); 
+}
+
+function piechart(years)
+{
+    console.log(`Bargraph(${years})`); 
+
+    d3.json(mydata).then((data) => {
+        
+        let gdpdata = data.map(object => object);
+        console.log("Here's the energy data=", gdpdata);
+        
+        function yearfilter(object) {return object.year == years}; //pulls out the id of 940
+        let yearArray = gdpdata.filter(yearfilter); //and puts it into the array
+        console.log("Here is my yearArray:", yearArray);
+        let gdpArray = yearArray.map(object => object.gdp);
+        console.log("Here is my GDPresultArray:", gdpArray);
+        for (let i = 0; i < gdpArray.length; i++) {
+            let gdp = gdpArray[i]; 
+            console.log("Here are the GDP of each state in 2010:", gdp);
+        };
+
+        // Call the Plotly function 
+        // Plotly.newPlot("bar", barArray, barLayout); 
+
+    }); 
+}
+function optionChanged(years)
+{
+    console.log(`optionChanged, new value: ${years}`); 
+
+    Bargraph(years); 
+    piechart(years); 
+    Metadata(years); 
+}
+
 
 // calling the function for the function to run.
 InitDashboard(); 
